71001b427ce8618c0c482491ced38b5ff06c3356
Nick De Veaux <ndeveaux@NickDeVeauxs15MacBookPro.local>
2016-06-20 14:51:26 -0400

    design and response comment

--- inferelator_strawman/design_and_response_driver.R
+++ inferelator_strawman/design_and_response_driver.R
@@ -1,3 +1,6 @@
+# Author: Nick De Veaux
+# Short R file for communicating between python processes and legacy R file design_and_response.R
+
 source('legacy/design_and_response.R')
 
 meta.data <- read.table('meta_data.csv', sep = ',', header = 1, row.names = 1)
@@ -11,7 +14,3 @@ dr$final_design_matrix
 
 write.table(as.matrix(dr$final_response_matrix), 'response.tsv', sep = '\t')
 write.table(as.matrix(dr$final_design_matrix), 'design.tsv', sep = '\t')
-
-# creates a thing called off. 
-# sets all delT larger to max
-# logically we don't expect this to filter
