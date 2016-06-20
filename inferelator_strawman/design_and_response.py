a785bfdbd406286a88e58ad8a0f962e9365fcd8c
Nick De Veaux <ndeveaux@NickDeVeauxs15MacBookPro.local>
2016-06-20 12:11:20 -0400

    legacy design response driver

--- inferelator_strawman/design_and_response.py
+++ inferelator_strawman/design_and_response.py
@@ -1,12 +1,29 @@
 import pandas
 import subprocess
+import os
 
 class Legacy_Design_Response_Driver:
 
-	def get_design_response(exp_mat, meta_data):
+	def get_design_response(self, exp_mat, meta_data):
+		# Change the current working directory
+		cwd = os.getcwd()
+		new_dir = os.path.dirname(os.path.abspath(__file__))
+		os.chdir(new_dir)
+		self.convert_to_R_df(meta_data)
 		exp_mat.to_csv('exp_mat.csv')
 		meta_data.to_csv('meta_data.csv')
+
 		subprocess.call(['R', '-f', './design_and_response_driver.R'])
 		final_design = pandas.read_csv('design.tsv', sep='\t')
 		final_response = pandas.read_csv('response.tsv', sep='\t')
+
+		# restored the current directory
+		os.chdir(cwd)
 		return (final_design, final_response)
+
+	def convert_to_R_df(self, df):
+		new_df = pandas.DataFrame(df)
+		import pdb; pdb.set_trace()
+		for col in new_df:
+			if new_df[col].dtype == 'bool':
+				new_df[col] = [str(x).upper() for x in new_df[col]]
--- inferelator_strawman/tests/test_design_response.py
+++ inferelator_strawman/tests/test_design_response.py
@@ -1,10 +1,28 @@
 import unittest
 from .. import design_and_response
+import pandas
+import numpy as np
+
 
 class TestDesignResponse(unittest.TestCase):
 
 	def test_nothing(self):
+		meta = pandas.DataFrame()
+		meta['isTs']=[True, True, True, True, False]
+  		meta['is1stLast'] = ['f','m','m','l','e']
+  		meta['prevCol'] = ['NA','ts1','ts2','ts3', 'NA']
+  		meta['del_t'] = ['NA', 1, 2, 5, 'NA']
+  		meta['condName'] = ['ts1','ts2','ts3','ts4','ss']
+  		exp = pandas.DataFrame(np.reshape(range(10), (2,5)) + 1,
+  		 index = ['gene' + str(i + 1) for i in range(2)],
+  		 columns = ['ts' + str(i + 1) for i in range(4)] + ['ss'])
+  		print meta
+  		ds, resp = design_and_response.Legacy_Design_Response_Driver().get_design_response(exp, meta)
 		self.assertTrue(False)
+
 
 	def test_num_columns_design_response(self):
 		self.assertTrue(False)
+
+
+
