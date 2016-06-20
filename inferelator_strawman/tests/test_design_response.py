16b5b011bf7d5b8896d2211ac56bdb29c3348e7f
Nick De Veaux <ndeveaux@NickDeVeauxs15MacBookPro.local>
2016-06-20 14:41:50 -0400

    calculated response for ts2 and ts3

--- inferelator_strawman/tests/test_design_response.py
+++ inferelator_strawman/tests/test_design_response.py
@@ -7,21 +7,21 @@ import numpy as np
 class TestDesignResponse(unittest.TestCase):
 
 	def setup(self):
-		meta = pandas.DataFrame()
-		meta['isTs']=[True, True, True, True, False]
-  		meta['is1stLast'] = ['f','m','m','l','e']
-  		meta['prevCol'] = ['NA','ts1','ts2','ts3', 'NA']
-  		meta['del.t'] = ['NA', 1, 2, 5, 'NA']
-  		meta['condName'] = ['ts1','ts2','ts3','ts4','ss']
+		self.meta = pandas.DataFrame()
+		self.meta['isTs']=[True, True, True, True, False]
+  		self.meta['is1stLast'] = ['f','m','m','l','e']
+  		self.meta['prevCol'] = ['NA','ts1','ts2','ts3', 'NA']
+  		self.meta['del.t'] = ['NA', 1, 2, 5, 'NA']
+  		self.meta['condName'] = ['ts1','ts2','ts3','ts4','ss']
 
   		self.exp = pandas.DataFrame(np.reshape(range(10), (2,5)) + 1,
   		 index = ['gene' + str(i + 1) for i in range(2)],
   		 columns = ['ts' + str(i + 1) for i in range(4)] + ['ss'])
   		delT_min = 2
 		delT_max = 4
-		tau = 2
+		self.tau = 2
 
-  		return design_and_response.Legacy_Design_Response_Driver().get_design_response(self.exp, meta, delT_min, delT_max, tau)
+  		return design_and_response.Legacy_Design_Response_Driver().get_design_response(self.exp, self.meta, delT_min, delT_max, self.tau)
 
 	def test_design_matrix(self):
 		# Set up variables 
@@ -40,11 +40,22 @@ class TestDesignResponse(unittest.TestCase):
 		self.assertEqual(list(ds['ss']), list(resp['ss']), msg = 'Steady State design and response should be equal')
 
 
-	def test_response_matrix(self):
+	def test_response_matrix_steady_state(self):
 		ds, resp = self.setup()
-		self.assertEqual(list(resp['ts4']), [4, 9])
-		self.assertEqual(list(resp['ts2']), list(self.exp['ts3']))
-		self.assertEqual(list(resp['ts1']), list(self.exp['ts2']), 
-			msg = 'In a time series the response should be the expression data at time t + 1')
+		self.assertEqual(list(resp['ts4']), list(self.exp['ts4']))
+		self.assertEqual(list(resp['ss']), list(self.exp['ss']))
+
+	def test_response_matrix_time_series(self):
+		ds, resp = self.setup()
+		expression_1 = np.array(list(self.exp['ts1']))
+		expression_2 = np.array(list(self.exp['ts2']))
+		expected_response_1 = expression_1 + self.tau * (expression_2 - expression_1) /  float(self.meta['del.t'][1])
+
+		expression_3 = np.array(list(self.exp['ts3']))
+		expected_response_2 = expression_2 + self.tau * (expression_3 - expression_2) /  float(self.meta['del.t'][2])
+
+		self.assertEqual(list(resp['ts1']), list(expected_response_1))
+		self.assertEqual(list(resp['ts2']), list(expected_response_2), 
+			msg = 'In a time series the response should follow the formula for the expected_response_2')
 
 
